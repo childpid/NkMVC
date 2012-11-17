22633e2c16215285dde7cba1624ad60477e6084c
childpid <childpid@gmail.com>
2012-11-17 22:31:18 +0100

    Added some controller methods

--- src/NkMVC/Modules/Defy/index.php
+++ src/NkMVC/Modules/Defy/index.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace NkMVC\Modules\Defy;
+
+use NkMVC\Legacy\Request\Request;
+use NkMVC\Legacy\Storage\DB;
+
+class Defy implements \NkMVC\Modules\ModuleInterface
+{
+    public function executeIndex(Request $request)
+    {
+        $db = DB::getInstance();
+        echo 'NkMV';
+        var_dump($db);
+
+    }
+
+    public function executeDownload(Request $request)
+    {
+        echo 'DOWNLOAD daaaa!';
+    }
+
+    public function getModuleName()
+    {
+        return __CLASS__;
+    }
+}
